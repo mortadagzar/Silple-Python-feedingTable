716c68e6b9c55bd2dc8299ca14ccf39431cf0efb
mortadagzar <36393184+mortadagzar@users.noreply.github.com>
2018-10-14 08:05:28 -0700

    Update settings.py

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -16,9 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'pagv6@fcut3uv%ik!7nfn$noen+hnj1x&@6=g(=v7d9&)ttga#'
 
